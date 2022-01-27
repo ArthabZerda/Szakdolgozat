9ca97bf96148e83292d95e319d45e900647ce051
ArthabZerda <b.arthab1022@gmail.com>
2022-01-27 11:55:44 +0100

    dispose exit on close

--- src/jaratfoglalo/JaratMegtekint.java
+++ src/jaratfoglalo/JaratMegtekint.java
@@ -310,11 +310,13 @@ public class JaratMegtekint extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(JaratMegtekint.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new JaratMegtekint().setVisible(true);
+                
             }
         });
     }
